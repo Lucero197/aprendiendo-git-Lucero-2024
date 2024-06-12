89db15ffcb901dcdea50783e33dd102c52cb49fe
Lautaro Lucero <lelucero@itel.edu.ar>
2024-06-12 15:54:11 -0300

    Agregar animacion javacrip al logo

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@ const nuevoTitulo = "Aprendiendo Git con JavaScript";
 const tituloElement = document.querySelector('.page-title');
 
 tituloElement.textContent = nuevoTitulo;
+
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const img = document.getElementById("chile-garnacho");
+    let position = 0;
+    let direction = 1;
+
+    function animate() {
+        position += direction * 2; // Cambia el valor 2 para ajustar la velocidad
+        img.style.left = position + "px";
+
+        // Cambiar dirección cuando alcanza los bordes de la ventana
+        if (position > window.innerWidth - img.width || position < 0) {
+            direction *= -1;
+        }
+
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+});
